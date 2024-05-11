0447deb298b9200f01b61e632b8fa6cd9bf6a4a1
SholaOti <shola.otitoju2018@gmail.com>
2024-05-11 19:32:46 +0300

    not found page

--- src/not-found/NotFound.jsx
+++ src/not-found/NotFound.jsx
@@ -1,10 +1,36 @@
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import sorobanBanner from "../assets/sorobanBanner.png";
+import { EmojiSad } from "iconsax-react";
 
 export default function NotFound() {
   return (
-    <div>
-      Page not found, return to the home Page
-      <NavLink to={"./"}>Go to home Page</NavLink>
+    <div className="overflow-x-hidden bg-gray-100 h-screen justify-center ">
+      <section className="relative py-12 sm:py-16 lg:pt-20 xl:pb-0">
+        <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="max-w-3xl mx-auto text-center">
+            <div className="flex flex-row mt-5 justify-center text-center items-center gap-2 ">
+              <EmojiSad size="42" color="#000000" />
+              <h1 className="text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-5xl lg:leading-tight font-pj">
+                Page not found!!!
+              </h1>
+            </div>
+
+            <div className="relative inline-flex mt-10 group">
+              <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+
+              <Link
+                to="./"
+                className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl "
+                role="button"
+              >
+                Return to homepage
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/pages/dapp-components/DappComponents.jsx
+++ src/pages/dapp-components/DappComponents.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const DappComponents = () => {
   return (
-    <section className="py-12 bg-gray-50 sm:py-16 lg:py-20">
+    <section className="py-12  sm:py-16 lg:py-20">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-bold text-gray-900 font-pj">
